dc81eeea7b0a4b155b79b6ea96d5244b7cc0ec20
AlexxxBor <127771460+AlexxxBor@users.noreply.github.com>
2024-12-06 17:21:45 +0300

    Update program.py
    Кто-то (или что-то) поменял наш файл.

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And i change it every day')
     print('This is an apple')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
